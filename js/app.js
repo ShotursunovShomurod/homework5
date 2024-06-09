9acddae39c2b6a0cf682608baea8a2a3ce33f577
ShotursunovShomurod <ziyodxon.i08@gmail.com>
2024-06-09 19:56:32 +0500

    Homework5

---
+++ js/app.js
@@ -0,0 +1,124 @@
+// 1- masala
+// {
+    // function kvadratPerimetri (a) {
+    //     return 4 * a;
+    // }
+    
+    // console.log(kvadratPerimetri(5));
+// }
+
+// 2- masala
+// {
+//     function kvadratYuzasi(a) {
+//         return a * a;
+//     }
+    
+//     console.log(kvadratYuzasi(5));
+// }
+
+// 3- masala
+// {
+//     function tortburchakYuzasi(a, b) {
+//         return a * b;
+//     }
+    
+//     function tortburchakPerimetri(a, b) {
+//         return 2 * (a + b);
+//     }
+
+//     console.log(tortburchakYuzasi(5, 10));
+//     console.log(tortburchakPerimetri(5, 10));
+    
+// }
+
+// 4- masala
+// {
+//     function aylaningUzunligi(d) {
+//         const pi = 3.14;
+//         return pi * d;
+//     }
+    
+//     console.log(aylaningUzunligi(10));    
+// }
+
+// 5- masala
+// {
+//     function kubHajmi(a) {
+//         return a ** 3;
+//     }
+    
+//     function kubTolaSirti(a) {
+//         return 6 * (a ** 2);
+//     }
+    
+//     console.log(kubHajmi(3));
+//     console.log(kubTolaSirti(3));    
+// }
+
+// 6- masala
+// {
+//     function paralelepipedHajmi(a, b, c) {
+//         return a * b * c;
+//     }
+    
+//     function paralelepipedTolaSirti(a, b, c) {
+//         return 2 * (a * b + b * c + a * c);
+//     }
+    
+//     console.log(paralelepipedHajmi(2, 3, 4)); 
+//     console.log(paralelepipedTolaSirti(2, 3, 4));     
+// }
+
+// 7- masala
+// {
+//     function doiraningUzunligi(R) {
+//         const pi = 3.14;
+//         return 2 * pi * R;
+//     }
+    
+//     function doiraningYuzasi(R) {
+//         const pi = 3.14;
+//         return pi * (R ** 2);
+//     }
+    
+//     console.log(doiraningUzunligi(5));
+//     console.log(doiraningYuzasi(5));    
+// }
+
+// 8- masala
+// {
+//     function ortaArifmetigi(a, b) {
+//         return (a + b) / 2;
+//     }
+    
+//     console.log(ortaArifmetigi(10, 20));    
+// }
+
+// 9- masala
+// {
+//     function ortaGeometrigi(a, b) {
+//         return Math.sqrt(a * b);
+//     }
+    
+//     console.log(ortaGeometrigi(9, 16));    
+// }
+
+// 10- masala
+// {
+//     function ikkitaSon(a, b) {
+//         const yigindisi = a + b;
+//         const kopaytmasi = a * b;
+//         const aKvadrati = a ** 2;
+//         const bKvadrati = b ** 2;
+        
+//         return {
+//             yigindisi,
+//             kopaytmasi,
+//             aKvadrati,
+//             bKvadrati
+//         };
+//     }
+    
+//     const natija = ikkitaSon(3, 4);
+//     console.log(natija);    
+// }
